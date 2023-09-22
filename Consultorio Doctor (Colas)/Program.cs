200465df95454fdf36b6a28a9d08bab7d461af8e
Israel Carreon <Israelcarreon992@gmail.com>
2023-09-21 19:03:30 -0600

    Agregar archivos de proyecto.

---
+++ Consultorio Doctor (Colas)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consultorio_Doctor__Colas_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int espera = 1;
+            Queue<string> Espera_pacientes = new Queue<string>();
+            Espera_pacientes.Enqueue("Paciente");
+            Espera_pacientes.Enqueue("Paciente");
+            Espera_pacientes.Enqueue("Paciente");
+            Espera_pacientes.Enqueue("Paciente");
+            Espera_pacientes.Enqueue("Paciente");
+            Console.WriteLine("== Turnos disponibles ==");
+            foreach (var pacientes in Espera_pacientes)
+            {
+                Console.WriteLine("-- " + pacientes + " " + espera);
+                espera++;
+            }
+            espera = 1;
+            string paciente_atendido = Espera_pacientes.Dequeue();
+            Console.WriteLine("\nPaciente atendido: " + paciente_atendido  + " " + espera + "\n");
+            Console.WriteLine("== Turnos disponibles ==");
+            foreach (var pacientes in Espera_pacientes)
+            {
+                Console.WriteLine("-- " + pacientes + " " + espera);
+                espera++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
